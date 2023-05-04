049e30ae68be8d9ae8ded3576f16ee11f75319c2
zer0-star <zer0star.65535@gmail.com>
2023-05-04 14:09:08 +0900

    use cabal instead of stack

--- flake.nix
+++ flake.nix
@@ -27,16 +27,18 @@
           # source-overrides = { };
           # overrides = self: super: { };
 
-          # devShell = {
-          #  # Enabled by default
-          #  enable = true;  
-          #
-          #  # Programs you want to make available in the shell.
-          #  # Default programs can be disabled by setting to 'null'
-          #  tools = hp: { fourmolu = hp.fourmolu; ghcid = null; };
-          #
-          #  hlsCheck.enable = true;
-          # };
+           devShell = {
+            # Enabled by default
+            # enable = true;
+
+            # Programs you want to make available in the shell.
+            # Default programs can be disabled by setting to 'null'
+            tools = hp: {
+              inherit (hp) hpack;
+            };
+
+            hlsCheck.enable = true;
+           };
         };
 
         # haskell-flake doesn't set the default package, but you can do it here.
